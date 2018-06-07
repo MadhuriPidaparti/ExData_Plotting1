9009225310e676675391934c5e417a73a9b643fc
MadhuriPidaparti <36885360+MadhuriPidaparti@users.noreply.github.com>
2018-06-07 18:22:47 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,8 +6,7 @@ Data <- read.table("household_power_consumption.txt", sep=";", header=TRUE,  na.
 
 sub <- Data[Data$Date == "1/2/2007" | Data$Date == "2/2/2007",]
 
-# Save this plot into a PNG file with width of 480 pixels and height of 480 pixels
-
+# Open graphics device
 
  png(filename="plot1.png", width=480, height=480,bg="white")
 
